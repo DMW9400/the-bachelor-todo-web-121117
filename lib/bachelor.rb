4757c612a3fbadbdaaaa3f73c4edf4e4247b0079
Matthew Welch <dmw9400@gmail.com>
2017-11-25 21:32:13 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -101,5 +101,5 @@ def get_average_age_for_season(data, season)
     average_sum += item
   end
   final_average = average_sum.to_f/return_value.length.to_f
-return final_average
+return final_average.round
 end
